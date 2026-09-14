b768b1b29e9647f8d1dafa6b2fea4f0223a807e9
MUHAMAD ALHASAN <129693424+mhe98xmuh@users.noreply.github.com>
2026-09-14 22:21:34 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,4 @@
-```kotlin
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -76,4 +76,3 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
 }
-```
