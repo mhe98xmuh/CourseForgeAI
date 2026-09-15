14edb864156f4ae9555f08aef80f02db4442fa07
MUHAMAD ALHASAN <129693424+mhe98xmuh@users.noreply.github.com>
2026-09-15 06:33:03 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,10 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
 
+    // مكتبات المشغل المطلوبة التي تم اكتشاف غيابها
+    implementation("androidx.media3:media3-exoplayer:1.3.1")
+    implementation("androidx.media3:media3-ui:1.3.1")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
